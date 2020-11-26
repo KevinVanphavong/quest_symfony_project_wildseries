7866904a4a3b5666e00175d48edb871bcea79273
KevinVanphavong <kevin45van@hotmail.fr>
2020-11-26 23:45:11 +0100

    quest5_challenge

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,14 +7,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ProgramController
+ * @Route("/programs", name="program_")
+ */
+
 Class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
             'website' => 'Wild Séries',
         ]);
     }
