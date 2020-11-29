3f6d451f71ee9526b1f924dce155cc09d12d9264
KevinVanphavong <kevin45van@hotmail.fr>
2020-11-29 18:07:26 +0100

    quest8_challenge_getDataWithDoctrine

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Program;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class CategoryController
+ * @Route("/category", name="category_")
+ */
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @return Response A response instance
+     */
+    public function index(): Response
+    {
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+        return $this->render(
+            'category/index.html.twig',
+            [
+                'categories' => $categories
+            ]
+        );
+    }
+
+    /**
+     * @Route("/{categoryName}", methods={"GET"}, name="show")
+     * @return Response A response instance
+     */
+    public function show($categoryName): Response
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findBy([
+                'name' => $categoryName,
+            ]);
+
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(
+                ['category'=> $category], ['id'=>'DESC'], 3
+            );
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No category "'.$categoryName.'" found in category\'s table.'
+            );
+        }
+        return $this->render(
+            'category/show.html.twig',
+            [
+                'programs' => $programs
+            ]
+        );
+    }
+}
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,23 +16,45 @@ use Symfony\Component\Routing\Annotation\Route;
 Class ProgramController extends AbstractController
 {
     /**
+     * Show all rows from Program’s entity
+     *
      * @Route("/", name="index")
+     * @return Response A response instance
      */
     public function index(): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
-        ]);
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        return $this->render(
+            'program/index.html.twig',
+            [
+                'website' => 'Wild Séries',
+                'programs' => $programs]
+        );
     }
 
     /**
-     * @Route("/{id}", name="show", methods={"GET"}, requirements={"id"="\d+"})
+     * Getting a program by id
+     *
+     * @Route("/show/{id<^[0-9]+$>}", name="show")
+     * @return Response
      */
-    public function show(int $id): Response
+    public function show(int $id):Response
     {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
+            'program' => $program,
             'id' => $id,
-            'website' => 'Wild Séries',
         ]);
     }
 }
