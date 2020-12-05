4e4179bc4a80f7c5060f116c348308081ce51051
KevinVanphavong <kevin45van@hotmail.fr>
2020-12-05 21:24:42 +0100

    quest12_challenge_crud

--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -2,31 +2,29 @@
 
 namespace App\Form;
 
-use App\Entity\Program;
+use App\Entity\Episode;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
-class ProgramType extends AbstractType
+class EpisodeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title',TextType::class)
-            ->add('summary',TextType::class)
-            ->add('poster',TextType::class)
-            ->add('country',TextType::class)
-            ->add('year',TextType::class)
-            ->add('category', null, ['choice_label' => 'name'])
+            ->add('number',TextType::class)
+            ->add('synopsis',TextType::class)
+            ->add('season',null, ['choice_label' => 'description'])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Program::class,
+            'data_class' => Episode::class,
         ]);
     }
 }
--- src/Form/SeasonType.php
+++ src/Form/SeasonType.php
@@ -2,31 +2,29 @@
 
 namespace App\Form;
 
-use App\Entity\Program;
+use App\Entity\Season;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
-class ProgramType extends AbstractType
+class SeasonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title',TextType::class)
-            ->add('summary',TextType::class)
-            ->add('poster',TextType::class)
-            ->add('country',TextType::class)
+            ->add('number',TextType::class)
             ->add('year',TextType::class)
-            ->add('category', null, ['choice_label' => 'name'])
+            ->add('description',TextType::class)
+            ->add('program',null, ['choice_label' => 'title'])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Program::class,
+            'data_class' => Season::class,
         ]);
     }
 }
